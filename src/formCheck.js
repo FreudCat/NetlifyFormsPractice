967f1bdffb641bb848233c7facde71431f757c84
= <anguyen031@gmail.com>
2022-01-12 15:26:04 -0800

    Testing verification checkbox

---
+++ src/formCheck.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("submitButton");
+const isChecked = document.getElementById("checkbox");
+const errorMessage = document.getElementById("errorMessage");
+
+btn.addEventListener("click", e => {
+  
+})
