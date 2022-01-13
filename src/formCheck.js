8fa8442c59736ad3071e047fa05076e943f4d3cd
= <anguyen031@gmail.com>
2022-01-12 16:40:41 -0800

    Add passthrough

--- src/formCheck.js
+++ src/formCheck.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("submitButton");
 const isChecked = document.getElementsByTagName("iframe")[0];
 const spanbox = document.getElementsByTagName("span");
 const actualBox = document.getElementsByClassName("recaptcha-checkbox");
-const ariaChecked = isChecked.getAttribute("aria-checked");
+const ariaChecked = isChecked.contentDocument;
 const errorMessage = document.getElementById("errorMessage");
 
 btn.addEventListener("click", e => {
