d80614748555682c42b12b70f3c9ed784d7531f7
= <anguyen031@gmail.com>
2022-01-12 16:08:49 -0800

    Testing verification checkbox

--- src/formCheck.js
+++ src/formCheck.js
@@ -1,8 +1,8 @@
 const btn = document.getElementById("submitButton");
-const isChecked = document.getElementsByTagName("iframe")[0].innerHTML;
+const isChecked = document.getElementsByTagName("iframe");
 const errorMessage = document.getElementById("errorMessage");
 
 btn.addEventListener("click", e => {
   e.preventDefault(); 
-  console.log(isChecked);
+  console.log(isChecked[0].innerHTML);
 })
