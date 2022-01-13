4c3009634784f6c242abb0966820161921752bbc
= <anguyen031@gmail.com>
2022-01-12 16:27:14 -0800

    Add passthrough

--- src/formCheck.js
+++ src/formCheck.js
@@ -1,10 +1,10 @@
 const btn = document.getElementById("submitButton");
 const isChecked = document.getElementsByTagName("iframe");
-const actualBox = document.getElementsByClassName("recaptcha-checkbox").length;
+const actualBox = document.getElementsByClassName("recaptcha-checkbox");
 // const ariaChecked = isChecked.getAttribute("aria-checked");
 const errorMessage = document.getElementById("errorMessage");
 
 btn.addEventListener("click", e => {
   e.preventDefault(); 
-  console.log(actualBox);
+  console.log(actualBox[0].innerHTML);
 })
