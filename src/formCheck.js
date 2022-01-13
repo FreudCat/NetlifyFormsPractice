f9ad9e91813fb2a2a28633d481747b380860203d
= <anguyen031@gmail.com>
2022-01-12 16:07:24 -0800

    Testing verification checkbox

--- src/formCheck.js
+++ src/formCheck.js
@@ -1,8 +1,8 @@
 const btn = document.getElementById("submitButton");
-const isChecked = document.getElementsByTagName("iframe");
+const isChecked = document.getElementsByTagName("iframe")[0].innerHTML;
 const errorMessage = document.getElementById("errorMessage");
 
 btn.addEventListener("click", e => {
   e.preventDefault(); 
-  console.log(isChecked.length);
+  console.log(isChecked);
 })
